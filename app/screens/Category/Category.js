dfbdb8510a05f4cee9f6a0687b82986687cb0dd6
ValikShv <bobrshv@gmail.com>
2021-11-26 17:26:24 +0200

    qwe

--- app/screens/Category/Category.js
+++ app/screens/Category/Category.js
@@ -1,33 +1,44 @@
 import React, { useEffect, useState } from "react";
 import { styles } from "./styles";
 import { getItems } from "../../store/actions/items";
-import {ArrowLeft, Carousel, Header, ItemList, Wrapper } from "../../components";
+import { ArrowLeft, Carousel, Header, ItemList, Wrapper } from "../../components";
 import { useDispatch, useSelector } from "react-redux";
 import { Text, TouchableOpacity, View } from "react-native";
 import FilterDown from "../../components/svgIcon/FilterDown";
 import ModalDropdown from "react-native-modal-dropdown";
+import Filter from "../../components/svgIcon/Filter";
 
 export const Category = ({ navigation, route }) => {
   const [selectItem, setSelectItem] = useState(route.params.title);
-  const [filterValue, setFilterValue] = useState('shit')
+  const [filterValue, setFilterValue] = useState("shit");
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getItems(route.params.title));
   }, [selectItem]);
 
-  const filterType = [qwe,qwe,qwe,qwe];
+  const filterType = [
+    {
+      title: "Sort: A-Z ᐁ",
+      value: "asc",
+    },
+    {
+      title: "Sort: Z-A ᐁ",
+      value: "desc",
+    }];
 
-  const Selector = () => {
-    return(
-      <TouchableOpacity>
-        {/*<Text>{filterValue}</Text>*/}
-        <ModalDropdown
-        options={[qwe,qwe,qwe]}
-        onSelect={(value)=>{}}
-        />
-      </TouchableOpacity>
-    )
-  }
+  // const Selector = (value1) => {
+  //   const [open, setOpen] = useState();
+  //   return (
+  //     // {
+  //     //   open ?
+  //     //     <View><Text>shit</Text></View>
+  //     //     :
+  //     //     <TouchableOpacity>
+  //     //       <Text>{value1}</Text>
+  //     //     </TouchableOpacity>
+  //     // }
+  //   );
+  // };
 
   const chooseCard = (id) => {
     navigation.navigate("CurrentItem", { id });
@@ -42,8 +53,8 @@ export const Category = ({ navigation, route }) => {
             func: () => navigation.goBack(),
           }}
           rightBtn={{
-            // icon: <Filter />,
-            icon: <Selector/>,
+            icon: <Filter />,
+            // icon: <Selector />,
             func: () => navigation.goBack(),
           }}
           title={"Category"}
